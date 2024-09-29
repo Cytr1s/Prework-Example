1703f75d809cdcdf18ff105904376dd7768f2908
Azul <Azul.CastilloGuti0808@gmail.com>
2024-09-29 20:36:15 +0000

    e

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 print("Hello Code2College, Welcome to Fall!")
 user_name = input ("What is your name?: ")
-input = ("To conclude, your name is: {user_name}")
+input = ("To conclude, your name is: [user_name]")
