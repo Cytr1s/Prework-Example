99c5fa95c2dc5ca0cf1866e62f4158561eb5cb45
Azul <Azul.CastilloGuti0808@gmail.com>
2024-09-29 20:51:24 +0000

    A

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 print("Hello Code2College, Welcome to Fall!")
 user_name = input ("What is your name?: ")
 input = (f"To conclude, your name is {user_name}?: ")
-    if yes or Yes():
+if yes or Yes:
         print("That's great, let's move one")
-    if no or No():
+if no or No:
         print("I'm sorry, what's your name again?")
-        retur to user_name
+        return to user_name
