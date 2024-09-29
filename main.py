65075522daec48c130b20627c555287828a58137
Azul <Azul.CastilloGuti0808@gmail.com>
2024-09-29 22:30:08 +0000

    a

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+usernames[]:
+    [{'username': amidvictoriouslybird, 'type': 'Shoes', 'price': 100.0, 'total': 20},
+      {'username': amidaffectionatewoot, 'type': 'Tshirts', 'price': 43.5, 'total': 32},
+      {'username': 2119, 'type': 'Pants', 'price': 34.0, 'total': 19},
+      {'username': 1194, 'type': 'Jumpers', 'price': 250.0, 'total': 5},  
+      {'username': 1300, 'type': 'Blouse', 'price': 24.76, 'total': 3},
+      {'username': 1118, 'type': 'Dress', 'price': 50.0, 'total': 10}, 
+      {'username': 1664, 'type': 'Suits', 'price': 250, 'total': 5}
+    ]
+
 print("Hello Code2College, Welcome to Fall!")
 user_name = input ("What is your name?: ")
 input(f"To conclude, your name is {user_name}?: ")
@@ -10,10 +20,12 @@ else:
 
 def user(age, name, user, password):
     user_input = input("How old are you?: ")
-    if user_age = <15
+    if user_age<15:
         print("Sorry. You\'re not old enough to Enter")
-    if user_age = x>15
-        print("Wec") 
+    if user_age>=15<18:
+        print("Welcome to Chatbot") 
+    if user_age = 15<age<18:
+        print("Welcome to Chatbot")
 
 def menu():
     print("1. Create a new account ")
@@ -37,4 +49,5 @@ def user_selection():
          print("\nSorry, Not a Valid Choice. Please try again!")
          return in_use
 
-def create_a_new_account():
+
+    
