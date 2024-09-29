3fc3f087e38eca74eeacf040298707aebfc3635d
Azul <Azul.CastilloGuti0808@gmail.com>
2024-09-29 21:04:12 +0000

    a

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 print("Hello Code2College, Welcome to Fall!")
 user_name = input ("What is your name?: ")
-user_input = (f"To conclude, your name is {user_name}?: ")
+input = (f"To conclude, your name is {user_name}?: ")
 if user_name == "Yes": # Corrected line
     print("Great!")
 if user_name == "No":
     input("What's your name then?: ")
+    break 
