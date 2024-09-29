fd4d4e769db3d2b9a857b2749b184a9965d27a23
Azul <Azul.CastilloGuti0808@gmail.com>
2024-09-29 20:23:23 +0000

    error

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ print("Hello Code2College, Welcome to Fall!")
 input ("How is your day going?: ")
 
 user_name = input (For personal reason, please add in your name: )
+input ("Your name is {user_name}")
