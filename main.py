482f4e8dedc63c680f5a3d0b9340d89bd7a610c2
Azul <Azul.CastilloGuti0808@gmail.com>
2024-09-29 21:33:33 +0000

    a

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ user_name = input ("What is your name?: ")
 input(f"To conclude, your name is {user_name}?: ")
 if user_name == "Yes" or "yes": # Corrected line
     print("Great! Let's move forward")
-if user_name == "No" or "no":
+elif user_name == "No" or "no":
     input("What's your name then?: ")
 else:
-    print("I'm sorry. That is not an answer, please try again:" )
-    
-     
+    print("I'm sorry. That is not an answer, please try again:")
+
+def assistance():
+      print("\n **GenZ STORE INVENTORY SYSTEM**")  
+        print("1. View Store Inventory ") 
+        print("2. Add A New Product")   
+        print("3. Remove Products")  
+        print("4. Exit\n")  
