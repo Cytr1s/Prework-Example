48df8b1cc72594064a2c211087859807ae3d23e6
Azul <Azul.CastilloGuti0808@gmail.com>
2024-09-29 21:20:25 +0000

    a

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ user_name = input ("What is your name?: ")
 input(f"To conclude, your name is {user_name}?: ")
 if user_name == "Yes": # Corrected line
     print("Great!")
-if user_name == "No":
+elif user_name == "No":
     input("What's your name then?: ")
 else:
     print("I'm sorry. That is not an answer, please try again:" )
-    return to input
+    
      
